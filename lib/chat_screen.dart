47fc0a860938dae7be51eb9a9a82fbbd298e7d3a
edaches <Abahedacheaustin@gmail.com>
2023-09-14 18:14:21 +0100

    chat screen completed

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/chatmessage.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -10,17 +11,30 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final TextEditingController _controller = TextEditingController();
+  final List<ChatMessage> _messages = [];
+
+  void _sendMessage() {
+    ChatMessage _message = ChatMessage(text: _controller.text, sender: "user");
+
+    setState(() {
+      _messages.insert(0, _message);
+    });
+
+    _controller.clear();
+  }
 
   Widget _buildTextComposer() {
     return Row(
       children: [
         Expanded(
           child: TextField(
+            controller: _controller,
+            onSubmitted: (value) => _sendMessage(),
             decoration: InputDecoration.collapsed(hintText: 'Send a message'),
           ),
         ),
         IconButton(
-          onPressed: () {},
+          onPressed: () => _sendMessage(),
           icon: Icon(Icons.send),
         )
       ],
@@ -31,15 +45,19 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ChatGPT Demo'),
+        title: Center(child: Text('iandaxGPT ')),
       ),
       body: SafeArea(
         child: Column(
           children: [
             Flexible(
-              child: Container(
-                height: context.screenHeight,
-              ),
+              child: ListView.builder(
+                  reverse: true,
+                  padding: Vx.m8,
+                  itemCount: _messages.length,
+                  itemBuilder: (context, index) {
+                    return _messages[index];
+                  }),
             ),
             Container(
               decoration: BoxDecoration(color: context.cardColor),
